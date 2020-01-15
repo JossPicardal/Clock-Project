8d4ae8885d3a74bbe4f1326a16a878ab57da2024
JossPicardal <57271278+JossPicardal@users.noreply.github.com>
2020-01-15 10:47:36 -1000

    Final Final Commit

--- app4.js
+++ app4.js
@@ -1,8 +1,29 @@
-function drawClock(){
-	drawFace(ctx, radius);
-}
-
-ctx.beginPath();
-ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-ctx.fillStyle = 'white';
-ctx.fill();
+ $(document).ready(function(){
+  setInterval(function(){
+    var seconds = new Date().getSeconds();
+    var sdegree = seconds * 6;
+    $('.second').css({
+       'transform': 'rotate('+ sdegree + 'deg)'
+    });
+  }, 1000);
+    
+  setInterval( function() {
+    var mins = new Date().getMinutes();
+    var mdegree = mins * 6;
+    $('.minute').css({
+      'transform': 'rotate('+ mdegree + 'deg)'
+    });
+  }, 1000);
+  
+  setInterval( function() {
+    var hours = new Date().getHours();   
+    var mins = new Date().getMinutes();
+    var hdegree = hours * 30 + (mins / 2);    
+    
+    $('.hour').css({
+      'transform': 'rotate('+ hdegree + 'deg)'
+    });
+    
+}, 1000); 
+  
+});
